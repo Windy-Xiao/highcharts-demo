d61c1a1cd12e67fc7ba7ee4eca3b4fa63ad515f4
yuwei xiao <yuwei.xiao@thoughtworks.com>
2021-12-31 12:01:12 +0800

    extract rewrite highcharts methods to highchartsRewrite.ts

--- src/pages/charts/Charts.tsx
+++ src/pages/charts/Charts.tsx
@@ -8,6 +8,12 @@ import Highcharts, {
   WrapProceedFunction,
 } from "highcharts";
 import HC from "highcharts-rounded-corners";
+import {
+  rewriteHide,
+  rewriteMove,
+  rewriteRefresh,
+  rewriteUpdatePosition,
+} from "./highchartsRewrite";
 HC(Highcharts);
 
 interface Colors {
@@ -159,81 +165,10 @@ const options: Options = {
 };
 
 const Charts = () => {
-  // rewrite highcharts methods for tooltip show
-  Highcharts.wrap(Highcharts.Tooltip.prototype, "hide", function (proceed) {});
-  Highcharts.wrap(Highcharts.Tooltip.prototype, "refresh", function (
-    proceed,
-    point,
-    event,
-    click
-  ) {
-    if (click) {
-      proceed.apply(this, Array.prototype.slice.call(arguments, 1));
-    }
-  } as WrapProceedFunction);
-  Highcharts.wrap(
-    Highcharts.Tooltip.prototype,
-    "updatePosition",
-    function (proceed, point) {
-      proceed.apply(this, Array.prototype.slice.call(arguments, 1));
-
-      this.label.attr({
-        anchorY: point.plotY + this.chart.plotTop,
-        anchorX: point.plotX + this.chart.plotLeft + 8,
-      });
-    }
-  );
-  // rewrite highcharts methods for tooltip anchor
-  Highcharts.wrap(Highcharts.Tooltip.prototype, "move", function (
-    proceed,
-    x,
-    y,
-    anchorX,
-    anchorY
-  ) {
-    var tooltip = this,
-      now = tooltip.now,
-      animate =
-        tooltip.options.animation !== false &&
-        !tooltip.isHidden &&
-        // When we get close to the target position, abort animation and
-        // land on the right place (#3056)
-        (Math.abs(x - now.x) > 1 || Math.abs(y - now.y) > 1),
-      skipAnchor = false; //tooltip.followPointer || tooltip.len > 1;
-
-    // Get intermediate values for animation
-    extend(now, {
-      x: animate ? (2 * now.x + x) / 3 : x,
-      y: animate ? (now.y + y) / 2 : y,
-      anchorX: skipAnchor
-        ? undefined
-        : animate
-        ? (2 * now.anchorX + anchorX) / 3
-        : anchorX,
-      anchorY: skipAnchor
-        ? undefined
-        : animate
-        ? (now.anchorY + anchorY) / 2
-        : anchorY,
-    });
-    // Move to the intermediate value
-    tooltip.getLabel().attr(now);
-
-    // Run on next tick of the mouse tracker
-    if (animate) {
-      // Never allow two timeouts
-      clearTimeout(this.tooltipTimeout);
-
-      // Set the fixed interval ticking for the smooth tooltip
-      this.tooltipTimeout = setTimeout(function () {
-        // The interval function may still be running during destroy,
-        // so check that the chart is really there before calling.
-        if (tooltip) {
-          tooltip.move(x, y, anchorX, anchorY);
-        }
-      }, 32);
-    }
-  } as WrapProceedFunction);
+  rewriteHide();
+  rewriteRefresh();
+  rewriteUpdatePosition();
+  rewriteMove();
 
   useEffect(() => {
     const chart = Highcharts.chart(options);
